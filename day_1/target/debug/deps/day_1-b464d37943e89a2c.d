3d3ed04bf22860c866d59099aa035774c57b74e9
Harrison Cook <HonsonCooky@gmail.com>
2023-12-06 17:45:47 +1300

    cleanup

--- day_1/target/debug/deps/day_1-b464d37943e89a2c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Honso\source\advent-of-code\day_1\target\debug\deps\day_1-b464d37943e89a2c.rmeta: src\main.rs
-
-C:\Users\Honso\source\advent-of-code\day_1\target\debug\deps\day_1-b464d37943e89a2c.d: src\main.rs
-
-src\main.rs:
