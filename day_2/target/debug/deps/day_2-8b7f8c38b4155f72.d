f22a98d39fde7c711cb01072e1c37b13d3882b6b
Harrison Cook <HonsonCooky@gmail.com>
2023-12-06 17:52:35 +1300

    start day_2

---
+++ day_2/target/debug/deps/day_2-8b7f8c38b4155f72.d
@@ -0,0 +1,5 @@
+C:\Users\Honso\source\advent-of-code\day_2\target\debug\deps\day_2-8b7f8c38b4155f72.rmeta: src\main.rs
+
+C:\Users\Honso\source\advent-of-code\day_2\target\debug\deps\day_2-8b7f8c38b4155f72.d: src\main.rs
+
+src\main.rs:
---
+++ day_2/target/debug/deps/day_2-a7815c3dcb1f1925.d
@@ -0,0 +1,5 @@
+C:\Users\Honso\source\advent-of-code\day_2\target\debug\deps\day_2-a7815c3dcb1f1925.rmeta: src\main.rs
+
+C:\Users\Honso\source\advent-of-code\day_2\target\debug\deps\day_2-a7815c3dcb1f1925.d: src\main.rs
+
+src\main.rs:
