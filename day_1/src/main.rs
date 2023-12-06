4c37fc34a7dd3aa1119eaa906417b2e41f3b39f6
Harrison Cook <HonsonCooky@gmail.com>
2023-12-06 17:45:17 +1300

    finish day_1

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,66 +1,11 @@
-use std::{fmt::format, fs, usize};
-
-struct Digit {
-    value: usize,
-    index: isize,
-    is_valid: bool,
-}
-
-struct Answer {
-    first: Digit,
-    second: Digit,
-    value: usize,
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-// PART ONE
-// --------------------------------------------------------------------------------------------------------------------
-
-fn find_first_digit(line: &String) -> Digit {
-    let index: isize = match line.find(|c: char| c.is_digit(10)) {
-        Some(i) => i as isize,
-        _ => -1,
-    };
-
-    let value = if index == -1 {
-        0
-    } else {
-        line.chars()
-            .nth(index as usize)
-            .unwrap()
-            .to_digit(10)
-            .unwrap() as usize
-    };
-
-    Digit {
-        value,
-        index,
-        is_valid: index != -1,
-    }
-}
-
-fn part_one(line: &String) -> Answer {
-    let first = find_first_digit(line);
-    let second = find_first_digit(&line.chars().rev().collect::<String>());
-    let value = format!("{}{}", first.value, second.value)
-        .parse::<usize>()
-        .unwrap_or(0);
-    Answer {
-        first,
-        second,
-        value,
-    }
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-// PART TWO
-// --------------------------------------------------------------------------------------------------------------------
+use core::panic;
+use std::fs;
 
 const NUM_STRS: [&str; 9] = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
 
-fn num_str_to_int(num: &str) -> usize {
+fn num_str_to_int(num: &str) -> isize {
     match num {
         "one" => 1,
         "two" => 2,
@@ -75,68 +20,105 @@ fn num_str_to_int(num: &str) -> usize {
     }
 }
 
-fn find_num_str(line: &String, rev: bool) -> Digit {
-    let mut best_match = (0, "");
-    for num in NUM_STRS {
-        if !line.contains(num) {
+fn first_digit(line: String) -> (isize, isize) {
+    match line.chars().enumerate().find(|(_, c)| c.is_digit(10)) {
+        Some((i, c)) => (i as isize, c.to_digit(10).unwrap() as isize),
+        None => (-1, 0),
+    }
+}
+
+fn last_digit(line: String) -> (isize, isize) {
+    match line.chars().rev().enumerate().find(|(_, c)| c.is_digit(10)) {
+        Some((i, c)) => (
+            (line.len() - i - 1) as isize,
+            c.to_digit(10).unwrap() as isize,
+        ),
+        None => (-1, 0),
+    }
+}
+
+fn first_word(line: String) -> (isize, isize) {
+    let mut best_match = (-1, "");
+    for word in NUM_STRS {
+        if !line.contains(word) {
             continue;
         }
 
-        if rev {
-            let index = line.rfind(num).unwrap();
-            if best_match.1 == "" || best_match.0 < index {
-                best_match = (index, num);
-            }
-        } else {
-            let index = line.find(num).unwrap();
-            if best_match.1 == "" || best_match.0 > index {
-                best_match = (index, num);
-            }
+        let index = line.find(word).unwrap() as isize;
+        if best_match.1 == "" || index < best_match.0 {
+            best_match = (index, word);
         }
     }
 
     if best_match.1 == "" {
-        Digit {
-            value: 0,
-            index: -1,
-            is_valid: false,
+        return (-1, 0);
+    }
+
+    let value = num_str_to_int(best_match.1);
+
+    (best_match.0, value)
+}
+
+fn last_word(line: String) -> (isize, isize) {
+    let mut best_match = (-1, "");
+    for word in NUM_STRS {
+        if !line.contains(word) {
+            continue;
+        }
+
+        let index = line.rfind(word).unwrap() as isize;
+        if best_match.1 == "" || index > best_match.0 {
+            best_match = (index, word);
+        }
+    }
+
+    if best_match.1 == "" {
+        return (-1, 0);
+    }
+
+    let value = num_str_to_int(best_match.1);
+
+    (best_match.0, value)
+}
+
+fn find_best(a: (isize, isize), b: (isize, isize), is_first: bool) -> isize {
+    if a.0 == -1 && b.0 == -1 {
+        panic!("Unable to find digit or word");
+    }
+
+    if is_first {
+        if a.0 != -1 && a.0 < b.0 {
+            a.1
+        } else if b.0 == -1 {
+            a.1
+        } else {
+            b.1
         }
     } else {
-        Digit {
-            value: num_str_to_int(best_match.1),
-            index: best_match.0 as isize,
-            is_valid: true,
+        if a.0 != -1 && a.0 > b.0 {
+            a.1
+        } else if b.0 == -1 {
+            a.1
+        } else {
+            b.1
         }
     }
 }
-
-fn part_two(line: &String) -> Answer {
-    let lower_case = line.to_lowercase();
-    let first = find_num_str(&lower_case, false);
-    let second = find_num_str(&lower_case, true);
-    let value = format!("{}{}", first.value, second.value)
-        .parse::<usize>()
-        .unwrap_or(0);
-
-    Answer {
-        first,
-        second,
-        value,
-    }
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-// MAIN
-// --------------------------------------------------------------------------------------------------------------------
-
-fn find_val(digits: Answer, words: Answer) {}
 
 fn main() {
     let file = fs::read_to_string("input.txt").unwrap();
     let mut sum = 0;
     for line in file.lines() {
-        let part_one = part_one(&String::from(line));
-        let part_two = part_two(&String::from(line));
+        let first_digit = first_digit(String::from(line));
+        let last_digit = last_digit(String::from(line));
+
+        let first_word = first_word(String::from(line));
+        let last_word = last_word(String::from(line));
+
+        let first = find_best(first_digit, first_word, true);
+        let last = find_best(last_digit, last_word, false);
+
+        sum += format!("{}{}", first, last).parse::<i32>().unwrap();
     }
 
     println!("Sum: {sum}");
