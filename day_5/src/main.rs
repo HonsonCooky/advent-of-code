d03db80f7f0ed47fdca6970a130a24cda2e09362
Harrison Cook <HonsonCooky@gmail.com>
2023-12-07 22:13:32 +1300

    start day_5

---
+++ day_5/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs;
+
+struct Mapping {
+    source: string,
+    destination: string,
+
+}
+
+fn main() {
+    let file = fs::read_to_string("sample.txt").unwrap();
+    let mut map_of_maps = 
+
+    for line in file.lines() {
+
+    }
+}
