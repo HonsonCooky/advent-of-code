13fcbda0d8eb1bdcba3fda6fd421e3b2ef4d5ed3
Harrison <harrison.cook@z.co.nz>
2023-12-07 14:56:39 +1300

    day_1 extended version

---
+++ day_1_extended/target/debug/day_1_extended.d
@@ -0,0 +1,1 @@
+C:\Users\harrison.cook\source\advent-of-code\day_1_extended\target\debug\day_1_extended.exe: C:\Users\harrison.cook\source\advent-of-code\day_1_extended\src\main.rs
---
+++ day_1_extended/target/debug/deps/day_1_extended-273909200a4710d2.d
@@ -0,0 +1,5 @@
+C:\Users\harrison.cook\source\advent-of-code\day_1_extended\target\debug\deps\day_1_extended-273909200a4710d2.rmeta: src\main.rs
+
+C:\Users\harrison.cook\source\advent-of-code\day_1_extended\target\debug\deps\day_1_extended-273909200a4710d2.d: src\main.rs
+
+src\main.rs:
