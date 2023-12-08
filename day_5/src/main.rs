d70d2f00cf3645d7f02a05344ada5888234a56b8
Harrison <harrison.cook@z.co.nz>
2023-12-08 15:42:50 +1300

    day_5 progress

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,16 +1,30 @@
-use std::fs;
+use std::{collections::HashMap, fs, iter::Peekable, path::Iter, str::Lines, usize, fmt::Display};
 
-struct Mapping {
-    source: string,
-    destination: string,
+fn get_seeds<'a, I>(lines: &mut I) -> Vec<usize> 
+where I:Iterator<Item = &'a str>, {
+    let line = lines.next().unwrap();
+    if !line.starts_with("seeds") {
+        panic!("Not a seeds line")
+    }
 
+    line.split(":")
+        .last()
+        .unwrap()
+        .trim()
+        .split(" ")
+        .map(|n| n.parse().unwrap())
+        .collect()
 }
+
+fn make_mapping(lines: Vec<String>) {}
 
 fn main() {
     let file = fs::read_to_string("sample.txt").unwrap();
-    let mut map_of_maps = 
+    let mut lines = file.lines();
+    let seeds = get_seeds(&mut lines);
 
-    for line in file.lines() {
-
+    while let Some(line) = lines.next() {
+        let partition = lines.partition(|line| )
+        println!("{:?}", collection);
     }
 }
