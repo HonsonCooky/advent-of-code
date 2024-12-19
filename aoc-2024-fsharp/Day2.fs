1b04b6c507a49823029f15779ebb1720616164ce
HarrisonCook <HonsonCooky@gmail.com>
2024-12-19 13:48:00 +1300

    Start day 2

--- aoc-2024-fsharp/Day2.fs
+++ aoc-2024-fsharp/Day2.fs
@@ -1,2 +1,12 @@
 module aoc_2024_fsharp.Day2
 
+open System
+let d2s1 () =
+    System.IO.File.ReadLines("./Examples/Day2.txt")
+    |> Seq.map _.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map (Seq.map int)
+    |> Seq.map (fun s -> Seq.take 2 s |> Seq.toList |> (fun s' -> if s'[0] < s'[1] then s else Seq.rev s))
+    |> Seq.map Seq.pairwise
+    |> Seq.filter (Seq.forall (fun (a, b) -> a - b >= 1 && a - b <= 3))
+    |> Seq.length
+    |> printfn("Day 2, Solution 1: %i")
--- aoc-2024-fsharp/Program.fs
+++ aoc-2024-fsharp/Program.fs
@@ -1,1 +1,3 @@
-﻿open
+﻿open aoc_2024_fsharp
+
+Day2.d2s1()
