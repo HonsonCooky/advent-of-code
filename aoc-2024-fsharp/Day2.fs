c2118758c60fa420ba07c84ad117a6a103f84f1a
HarrisonCook <HonsonCooky@gmail.com>
2024-12-20 14:46:32 +1300

    Day 2 Solution 2 - Stuck

--- aoc-2024-fsharp/Day2.fs
+++ aoc-2024-fsharp/Day2.fs
@@ -1,12 +1,26 @@
 module aoc_2024_fsharp.Day2
 
 open System
+
 let d2s1 () =
-    System.IO.File.ReadLines("./Examples/Day2.txt")
+    System.IO.File.ReadLines("./Inputs/Day2.txt")
     |> Seq.map _.Split(" ", StringSplitOptions.RemoveEmptyEntries)
     |> Seq.map (Seq.map int)
     |> Seq.map (fun s -> Seq.take 2 s |> Seq.toList |> (fun s' -> if s'[0] < s'[1] then s else Seq.rev s))
     |> Seq.map Seq.pairwise
-    |> Seq.filter (Seq.forall (fun (a, b) -> a - b >= 1 && a - b <= 3))
+    |> Seq.filter (Seq.forall (fun (a, b) -> b - a >= 1 && b - a <= 3))
     |> Seq.length
-    |> printfn("Day 2, Solution 1: %i")
+    |> printfn ("Day 2, Solution 1: %i")
+
+let d2s2 () =
+
+    let isSafeReport (report: int List) =
+        true
+
+    System.IO.File.ReadLines("./Inputs/Day2.txt")
+    |> Seq.map _.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map (Seq.map int)
+    |> Seq.map Seq.toList
+    |> Seq.filter isSafeReport
+    |> Seq.length
+    |> printfn "Day 2, Solution 2: %i"
--- aoc-2024-fsharp/Program.fs
+++
@@ -1,3 +0,0 @@
-﻿open aoc_2024_fsharp
-
-Day2.d2s1()
