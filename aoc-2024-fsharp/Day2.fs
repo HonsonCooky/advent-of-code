564bcdc4d91e02c61325375d9a5fb60a6a3d049d
Harrison Cook <HonsonCooky@gmail.com>
2024-12-21 23:21:26 +1300

    Stuck Day 2, Solution 2

--- aoc-2024-fsharp/Day2.fs
+++ aoc-2024-fsharp/Day2.fs
@@ -14,13 +14,38 @@ let d2s1 () =
 
 let d2s2 () =
 
+    let isReportOrdered (report: int List) =
+        let rec checkOrder list errCount =
+            match list with
+            | []
+            | [ _ ] -> true
+            | x :: y :: rest when x < y -> checkOrder (y :: rest) errCount
+            | x :: y :: rest ->
+                match errCount with
+                | 1 -> false
+                | _ -> checkOrder (x :: rest) (errCount + 1) || checkOrder (y :: rest) (errCount + 1)
+
+        checkOrder report 0 || checkOrder (List.rev report) 0
+
     let isSafeReport (report: int List) =
-        true
+        let rec checkSafety list errCount =
+            match list with
+            | []
+            | [ _ ] -> true
+            | x :: y :: rest when y - x >= 1 && y - x <= 3 -> checkSafety (y :: rest) errCount
+            | x :: y :: rest ->
+                match errCount with
+                | 1 -> false
+                | _ -> checkSafety (x :: rest) (errCount + 1) || checkSafety (y :: rest) (errCount + 1)
+        
+        checkSafety report 0 || checkSafety (List.rev report) 0
 
     System.IO.File.ReadLines("./Inputs/Day2.txt")
     |> Seq.map _.Split(" ", StringSplitOptions.RemoveEmptyEntries)
     |> Seq.map (Seq.map int)
     |> Seq.map Seq.toList
+    |> Seq.filter isReportOrdered
     |> Seq.filter isSafeReport
+    |> fun s -> Seq.iter (printfn "Valid: %A") s; s
     |> Seq.length
     |> printfn "Day 2, Solution 2: %i"
